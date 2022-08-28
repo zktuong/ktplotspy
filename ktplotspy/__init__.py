15ec2e6717eb62f9bceff6b7f91f5dd4f2ceaf59
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-28 03:12:07 +0100

    Update __init__.py

--- ktplotspy/__init__.py
+++ ktplotspy/__init__.py
@@ -3,5 +3,4 @@ import toml
 from pathlib import Path
 
 from ktplotspy.plot import *
-
-__version__ = toml.load(Path(__file__).parent.parent / "pyproject.toml")["project"]["version"]
+from ktplotspy.utils.settings import __version__
