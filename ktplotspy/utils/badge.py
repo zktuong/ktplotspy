ac17a9ad970433fb2dffc241f680a59c1415e2b1
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-08-28 03:07:52 +0100

    hmm

--- ktplotspy/utils/badge.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-from ktplotspy import __version__
-
-print(__version__)
--- ktplotspy/utils/settings.py
+++ ktplotspy/utils/settings.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("ktplotspy").version
 
 DEFAULT_SEP = ">@<"
 DEFAULT_SPEC_PAT = "/|:|\\?|\\*|\\+|\\|\\(|\\)|\\/"
