fc5860aadc975c5b4b05f8b742823dfd95423aa8
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-11-30 22:15:49 +0000

    Update test_plot_cpdb_chord.py

--- tests/test_plot_cpdb_chord.py
+++ tests/test_plot_cpdb_chord.py
@@ -96,3 +96,70 @@ def test_plot_cpdb_chord_adata_col(mock_show, adata, means, pvals, decon):
         genes=["PTPRC", "TNFSF13", "BMPR2"],
     )
     g
+
+
+@patch("matplotlib.pyplot.show")
+@pytest.mark.usefixtures("adata", "means", "pvals", "decon")
+def test_plot_cpdb_chord_adata_col_nocat(mock_show, adata, means, pvals, decon):
+    adata.uns["celltype_colors"] = [
+        "#1f77b4",
+        "#ff7f0e",
+        "#279e68",
+        "#d62728",
+        "#aa40fc",
+        "#8c564b",
+        "#e377c2",
+        "#b5bd61",
+        "#17becf",
+        "#aec7e8",
+        "#ffbb78",
+        "#98df8a",
+    ]
+    adata.obs["celltype"] = [str(c) for c in adata.obs["celltype"]]
+    g = plot_cpdb_chord(
+        adata=adata,
+        cell_type1="B cell",
+        cell_type2=".",
+        means=means,
+        pvals=pvals,
+        deconvoluted=decon,
+        celltype_key="celltype",
+        genes=["PTPRC", "TNFSF13", "BMPR2"],
+    )
+    g
+
+
+@patch("matplotlib.pyplot.show")
+@pytest.mark.usefixtures("adata", "means", "pvals", "decon")
+def test_plot_cpdb_chord_adata_layer1(mock_show, adata, means, pvals, decon):
+    adata.layers["test"] = adata.X.copy()
+    g = plot_cpdb_chord(
+        adata=adata,
+        cell_type1="B cell",
+        cell_type2=".",
+        means=means,
+        pvals=pvals,
+        deconvoluted=decon,
+        celltype_key="celltype",
+        genes=["PTPRC", "TNFSF13", "BMPR2"],
+        layers="test",
+    )
+    g
+
+
+@patch("matplotlib.pyplot.show")
+@pytest.mark.usefixtures("adata", "means", "pvals", "decon")
+def test_plot_cpdb_chord_adata_layer2(mock_show, adata, means, pvals, decon):
+    adata.layers["test"] = adata.X.toarray().copy()
+    g = plot_cpdb_chord(
+        adata=adata,
+        cell_type1="B cell",
+        cell_type2=".",
+        means=means,
+        pvals=pvals,
+        deconvoluted=decon,
+        celltype_key="celltype",
+        genes=["PTPRC", "TNFSF13", "BMPR2"],
+        layers="test",
+    )
+    g
