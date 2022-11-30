d23570e230d278fb856916e8a59f1fa17bdb4867
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-11-30 22:08:18 +0000

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python
-from .fixtures.fixtures import adata, means, pvals, means_split, pvals_split
+from .fixtures.fixtures import adata, means, pvals, decon, means_split, pvals_split, decon_split
